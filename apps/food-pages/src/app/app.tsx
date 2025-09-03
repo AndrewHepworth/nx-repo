19ac34c6032ff28a21c0096e7145e2c57ffbc068
andrew.hepworth <andrew.hepworth@agsenze.com>
2025-09-03 13:06:56 +0100

    cleaned up ts configs a lil and put in webpage structure

--- apps/food-pages/src/app/app.tsx
+++ apps/food-pages/src/app/app.tsx
@@ -3,10 +3,97 @@ import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
 
+const dataSet = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+]
+
+function FilterableProductTable ({ data }: any) {
+
+  return ( 
+    <>
+      <div>
+        <SearchBar />
+        <div> </div>
+        <ProductTable data={data}/>
+      </div>
+    </>
+  )
+
+}
+
+
+function SearchBar () {
+
+  return (
+    <>
+      <div>
+        <search>
+          <form>
+            <input placeholder='Search...' /> 
+          </form>
+        </search>
+      </div>
+      <div> 
+        <input type='radio' /> 
+        Only show products in stock
+      </div>
+    </>
+  )
+}
+
+function ProductTable ({ data } ) {
+  let rows: any[] = [];
+  let lastCategory = null
+  data.forEach(element => {
+    if ( element.category !== lastCategory ) {
+      rows.push( <ProductCategoryRow  category={element.category} />)
+      lastCategory = element.category
+    }
+    rows.push(  
+      <ProductRow name={element.name} price={element.price} />
+    )
+  });
+
+  return (
+    <table> 
+      <thead>
+        <tr>
+          <th> Name </th> <th> Price </th>
+        </tr>
+      </thead>
+      <tbody> {rows} </tbody>
+    </table>
+  )
+}
+
+
+function ProductCategoryRow ({ category } : any) {
+
+  return ( 
+      <h3> {category } </h3>
+  )
+}
+
+
+function ProductRow ({ name, price } : any) {
+
+  return ( 
+    <tr> 
+      <th>{name} </th>
+      <th>{price} </th>
+    </tr> 
+  )
+}
+
 export function App() {
   return (
     <div>
-      <NxWelcome title="food-pages" />
+      <FilterableProductTable data={dataSet}/>
     </div>
   );
 }
